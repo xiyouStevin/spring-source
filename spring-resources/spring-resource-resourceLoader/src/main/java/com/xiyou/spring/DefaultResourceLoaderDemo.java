6ee1bcc97323c0792403923880dd79ecf773e05c
dengwenghan <dengwenhan@idea-group.cn>
2023-12-18 18:14:45 +0800

    批量获取某个文件夹下所有的文件

--- spring-resources/spring-resource-resourceLoader/src/main/java/com/xiyou/spring/DefaultResourceLoaderDemo.java
+++ spring-resources/spring-resource-resourceLoader/src/main/java/com/xiyou/spring/DefaultResourceLoaderDemo.java
@@ -16,7 +16,15 @@ public class DefaultResourceLoaderDemo {
         System.out.println("类是否存在 = " + classResource.exists());
 
         //加载文件系统中的资源
-        Resource fileResource = loader.getResource("file:/spring-source/spring-resources/spring-resource-resourceLoader/myfile.txt");
+        /**
+         * D:/A/B/B/myfile.txt这个是绝对路径，如果工作目录是D:/,那么相对路径就为./A/B/C/myfile.txt
+         * 如果我的工作目录是D:/A,那么相对路径就是./B/C/myfile.txt
+         * 如果我的龚总目录是D:/A/B/C/222.txt，那么相关路径表示为.../222.txt
+         */
+        Resource fileResource = loader.getResource("https://github.com/xiyouStevin/spring-source/blob/resource_code/spring-resources/spring-resource-resourceLoader/src/main/myfile.txt");
         System.out.println("文件是否存在 = " + fileResource.exists());
+
+        Resource filesResource = loader.getResource("file:D:\\code\\github\\spring-source\\spring-resources\\spring-resource-resourceLoader\\src\\main\\myfile.txt");
+        System.out.println("文件是否存在 = " + filesResource.exists());
     }
 }
